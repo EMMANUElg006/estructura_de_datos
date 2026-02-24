017ae881c9c949939deb00867aff1cf110a731c2
Emmanuel guerrero <emanuelguerrero006@gmail.com>
2026-02-24 07:55:28 -0500

    sistemas de contactos usando listas simples

---
+++ lista simple/sistema-contactos/src/Nodo.java
@@ -0,0 +1,8 @@
+public class Nodo {
+    private contaco dato ;
+    Nodo siguiente;
+
+    public Nodo(contaco dato) {
+        this.dato = dato;
+    }
+}
---
+++ lista simple/sistema-contactos/src/contaco.java
@@ -0,0 +1,7 @@
+public class contaco {
+   private String nombre;
+    private String telefono;
+    private String correo;
+
+
+}
