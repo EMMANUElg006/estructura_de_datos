bdf9b2566b05d95707e05006316306d7092fb2ee
emmanuel guerrero <emanuelguerrero006@gmail.com>
2026-05-24 19:49:18 -0500

    Sele agrego funcionalidad al proyecto

--- lista simple/sistema-contactos/src/Nodo.java
+++ lista simple/sistema-contactos/src/Nodo.java
@@ -1,7 +1,27 @@
-public class Nodo {
-    Nodo siguiente;
-    
-    public Nodo(contaco dato) {
+public class nodo {
+    private contacto dato;
+   private  nodo siguiente;
+
+
+    public contacto getDato() {
+        return dato;
+    }
+
+    public void setDato(contacto dato) {
         this.dato = dato;
     }
+
+    public nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public nodo(contacto dato ){
+        this.dato= dato;
+    }
 }
+
+
